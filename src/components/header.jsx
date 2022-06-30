f8490d78ae5281083f6657305b088db72c2ab04a
Gulaya-Musayeva <gmusayeva449@gmail.com>
2022-07-01 02:51:47 +0400

    navbar ve header yaradildi

---
+++ src/components/header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Typed from 'react-typed'
+import pic from '../laptop1.jpg'
+
+const header=() =>{
+  return (
+    <div className="header-wrapper">
+        <div className="main-info">
+            <h1>Web development and website services</h1>
+            <Typed className='typed-text'
+            strings={[
+                'Microsoft Azure Developing Associate',
+                'Microsoft Certified Trainer',
+                '88.8% MERN success rateTrainer']}
+                typeSpeed={40}
+                backSpeed={50}
+                loop>
+                
+            </Typed>
+            <a className="downloadcv" href={pic}>Download CV</a>
+
+
+        </div>
+
+    </div>
+  )
+}
+
+export default header
